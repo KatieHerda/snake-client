bee3ee9538fe73937d81a0f710ebc199b28958f1
Katie <katie.herda2@gmail.com>
2021-09-22 20:05:59 +0000

    created a client file. refactored the require/exports to accept/transmit objects.

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const net = require("net");
+const { connect } = require("./client")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: '50542',
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log('I\'m connected.');
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
